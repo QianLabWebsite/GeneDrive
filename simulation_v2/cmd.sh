3cf762ce51fd994e1296273a2391bd163c54aeaa
Jiao Bingke <41976574+JiaoBingke@users.noreply.github.com>
2024-01-25 19:04:55 +0800

    Update cmd.sh

--- simulation_v2/cmd.sh
+++ simulation_v2/cmd.sh
@@ -1,2 +1,2 @@
-#To produce the Fig 6a, run
+#1. To produce the Fig 6a, run
 python CAIN_modification_male_cut_simulation.py CAIN.Fig6a #To change the parameters, such as environment capacity, inbreeding coefficient, you can edit script before running
