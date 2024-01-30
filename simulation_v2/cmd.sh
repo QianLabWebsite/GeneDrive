0d2029213b024b5d8029123325d9d435a218dd8f
Jiao Bingke <41976574+JiaoBingke@users.noreply.github.com>
2024-01-30 14:09:34 +0800

    Delete simulation_v2/cmd.sh

--- simulation_v2/cmd.sh
+++
@@ -1,2 +0,0 @@
-#1. To produce the Fig 6a, put the CAIN_modification_male_cut_simulation.py and the CAIN_viability_and_inbreeding.py in one directory, and then run
-python CAIN_modification_male_cut_simulation.py CAIN.Fig6a #To change the parameters, such as environment capacity, inbreeding coefficient, you can edit script before running
